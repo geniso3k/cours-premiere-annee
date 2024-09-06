2076098eb82c453c3eed91ed298149180e9548e7
Genis GB. BYTYQI <genis@sio.local>
2024-09-06 11:11:40 +0200

    Fin avec fonction

--- index.js
+++ index.js
@@ -7,4 +7,29 @@ let etudiant = {
     age : new Date().getFullYear() - 2001,
     section : "BTS SIO "
 };
-console.log(etudiant);
+
+let lesEtudiants = new Array();
+lesEtudiants[0] = etudiant;
+
+lesEtudiants[1] = {
+    nom : "BYTYQI2 ",
+    prenom : "Genis2 ",
+    civilite : "Mr. ",
+    age : new Date().getFullYear() - 2001,
+    section : "BTS SIO "
+
+};
+
+afficherEtudiants(lesEtudiants);
+
+//Fonction afficheEtudiants
+function afficherEtudiants(tableau){
+
+    let cases = tableau.length;
+    for(i =0;i<cases;i++){
+        let elementSelectionne = tableau[i].nom;
+        console.log(elementSelectionne);
+    }
+
+
+}
