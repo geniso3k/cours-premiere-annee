1ec278c41682856a749213fbd7fb0557907bf8a2
Genis GB. BYTYQI <genis@sio.local>
2024-09-06 09:31:47 +0200

    Toutes les valeurs de l'objet etudant

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-//Stocker les informations d'un étudiant
-let nom = "BYTYQI ";
-let prenom = "Genis ";
-let civilite = "Mr. ";
-let age = new Date().getFullYear() - 2001;
-let section = "BTS SIO";
-console.log(civilite + nom + prenom);
 
 //Création d'un objet vide
-let etudiant = new Object();
+let etudiant = {};
+etudiant.nom = "BYTYQI ";
+etudiant.prenom = "Genis ";
+etudiant.civilite = "Mr. "
+etudiant.age = new Date().getFullYear() - 2001;
+etudiant.section = "BTS SIO ";
 console.log(etudiant);
